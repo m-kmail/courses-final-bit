bee15f71d9af215b48df45300a2c5acf119630fc
ezzat <ezatce117@outlook.com>
2022-09-07 00:53:56 +0300

    activate student moodle page

--- src/components/StudentMoodle.js
+++ src/components/StudentMoodle.js
@@ -7,59 +7,17 @@ class StudentMoodle extends Component {
     super();
 
     this.state = {
+      courses: [],
       searchStuts: "",
-      courses: [
-        { courseName: "css", status: "In progress" },
-        { courseName: "html", status: "past" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "past" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "past" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-        { courseName: "java", status: "In progress" },
-      ],
-      inprogress: [],
-      past: [],
     };
   }
 
   courseFilter = (e) => {
     this.setState({ searchStuts: e.target.value });
-    let progress = [];
-    let past = [];
-    this.state.courses.map((t) => {
-      if (t.status === "In progress") {
-        progress.push(t);
-      } else {
-        past.push(t);
-      }
-    });
-    this.setState({ inprogress: progress, past: past });
   };
 
   async getCourses() {
-    let courses = await axios.get();
+    let courses = await axios.get("http://localhost:5000/courses");
     return courses;
   }
 
@@ -80,32 +38,26 @@ class StudentMoodle extends Component {
   }
   render() {
     return (
-      <div>
-        <div className="coursesContainer">
-          <select
-            className="typeCourseInput"
-            value={this.state.searchStuts}
-            onChange={this.courseFilter}
-          >
-            <option value="" disabled selected>
-              Courses
-            </option>
-            <option value="in progress">In progress</option>
-            <option value="past">past</option>
-          </select>
-          <div className="courses">
-            {this.state.searchStuts === "in progress"
-              ? this.state.inprogress.map((t) => (
-                  <div className="courseDiv">
-                    <h3> {t.courseName}</h3>
-                  </div>
-                ))
-              : this.state.past.map((t) => (
-                  <div className="courseDiv">
-                    <h3> {t.courseName}</h3>
-                  </div>
-                ))}
-          </div>
+      <div className="coursesContainer">
+        <select
+          className="typeCourseInput"
+          value={this.state.searchStuts}
+          onChange={this.courseFilter}
+        >
+          <option value="" disabled selected>
+            Courses
+          </option>
+          <option value="in progress">In progress</option>
+          <option value="past">past</option>
+        </select>
+        <div className="courses">
+          {this.state.courses
+            .filter((course) => course.Status == this.state.searchStuts)
+            .map((t) => (
+              <div className="courseDiv">
+                <h3> {t.Name}</h3>
+              </div>
+            ))}
         </div>
       </div>
     );
